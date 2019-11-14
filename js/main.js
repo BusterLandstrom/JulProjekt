7c1a9ec246f6a99ac19d8a8e2f890d837ad81c1f
ViktorLandstrom <viktor.landstrom@elev.ga.ntig.se>
2019-11-14 16:38:17 +0100

    Started Menu layout buidling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const WIDTH = 1024;
 const HEIGHT = 768;
 
-let canvas = document.createElement('canvas');
+let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 canvas.setAttribute("class", "border");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
+
+let startButtonHeight = 300;
+let startButtonWidth = 80;
+let startX = 360;
+let startY = 270;
 
 ctx.beginPath();
 ctx.strokeStyle = 'brown';
@@ -13,19 +18,42 @@ ctx.lineWidth = '5';
 ctx.rect(0,0,1024,768);
 ctx.stroke();
 
-buttonGame = function(ctx, color, lwidth, lheight, x,y){
-ctx.beginPath();
-ctx.strokeStyle = color;
-ctx.lineWidth = '1';
-ctx.rect(x,y,lwidth,lheight);
-ctx.stroke();
-this.clicked=function(){
-    ctx.fillStyle='#ff0000'
-    ctx.fill();
-  }
-}
+canvas.addEventListener('click', function() { }, false);
 
-let startButton = new buttonGame(ctx, 'brown', 250, 40, 380, 280);
+createButton = function(img, x, y, dheight, dwidth){
+ctx.drawImage(img, x, y, dheight, dwidth);
+ctx.stroke();
+};
+
+let img = document.getElementById('startGame')
+let startButton = new createButton( img, startX, startY, startButtonHeight, startButtonWidth);
+
+var e = document.getElementById('myCanvas'),
+   elemLeft = e.offsetLeft,
+   elemTop = e.offsetTop,
+   elements = [];
+
+e.addEventListener('click', function(event) {
+   var xVal = event.pageX - elemLeft,
+   yVal = event.pageY - elemTop;
+   console.log(xVal, yVal);
+   elements.forEach(function(ele) {
+      if (yVal > ele.top && yVal < ele.top + ele.height && xVal > ele.left && xVal < ele.left + ele.width) {
+         alert('element clicked');
+      }
+   });
+}, false);
+elements.push({
+   colour: '#1C2128',
+   width: 250,
+   height: 200,
+   top: 30,
+   left: 20
+});
+elements.forEach(function(ele) {
+   ctx.fillStyle = element.colour;
+   ctx.fillRect(ele.left, ele.top, ele.width, ele.height);
+});
 
 let main = document.getElementsByTagName('main')[0];
 main.appendChild(canvas);
